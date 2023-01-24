375f3c5d189a5796c9a2dc81bee0c81b0ec6b2f7
benganellison <bengt.ellison@gmail.com>
2023-01-24 13:57:28 +0100

    first two days

---
+++ 2022/2/part1.py
@@ -0,0 +1,55 @@
+def score_strategy(strategy: str) -> int:
+    """Calculates the total score for a Rock Paper Scissors strategy.
+
+    Args:
+        strategy: A string containing the strategy guide. Each line should contain
+            two characters: the first character is the opponent's choice and the
+            second character is the player's choice.
+
+    Returns:
+        The total score for the strategy.
+    """
+    score = 0
+    for line in strategy.strip().split('\n'):
+        # Parse the opponent's choice and the player's choice from each line.
+        opponent, player = line[0], line[-1]
+        player_score = 0
+        round_score = 0
+
+        # Calculate the score for the player's choice.
+        if player == 'X':
+            player_score = 1
+        elif player == 'Y':
+            player_score = 2
+        elif player == 'Z':
+            player_score = 3
+
+        # Calculate the score for the round outcome.
+        if (opponent, player) in [('A', 'X'), ('B', 'Y'), ('C', 'Z')]:
+            round_score = 3
+        elif (opponent, player) in [('A', 'Y'), ('B', 'Z'), ('C', 'X')]:
+            round_score = 6
+        else:
+            round_score = 0
+
+        # Add the score for this round to the total score.
+        score += player_score + round_score
+
+    return score
+
+# Read the strategy from the input file.
+with open('test_input.txt') as f:
+    strategy = f.read()
+
+# Calculate and print the total score for the strategy.
+total_score = score_strategy(strategy)
+assert total_score == 15
+print("tests passed")
+
+# Read the strategy from the input file.
+with open('input.txt') as f:
+    strategy = f.read()
+
+# Calculate and print the total score for the strategy.
+total_score = score_strategy(strategy)
+print("final result: ", total_score)
