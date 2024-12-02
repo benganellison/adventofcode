56588231b556cb7a8d081e3e13a65fc2f678f4a1
benganellison <bengt.ellison@gmail.com>
2024-12-02 16:16:51 +0100

    improve template

---
+++ 2024/setup.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env zsh
+
+# Find the highest numbered folder
+highest=$(ls -d [0-9][0-9] 2>/dev/null | sort -n | tail -n 1)
+
+# Determine the next folder number
+if [[ -z "$highest" ]]; then
+    next="01"
+else
+    next=$(printf "%02d" $((10#$highest + 1)))
+fi
+
+# Create the next folder
+mkdir "$next"
+
+cp -R template/* "$next"
+
+cd "$next"
+uv venv -p python3.12
+source .venv/bin/activate
+uv pip install -r requirements.txt
