e4b985ae2b2b3b0b4f83234f085b007a23211d3f
benganellison <bengt.ellison@gmail.com>
2025-12-04 07:00:39 +0100

    clean up for performance

--- 2025/04/part1.py
+++ 2025/04/part1.py
@@ -4,8 +4,9 @@ def parse_input_map(input):
 
 def main(input):
     paper_rolls = parse_input_map(input)
-    print(paper_rolls)
     result = 0
+    max_row_ind = len(paper_rolls)
+    max_col_ind = len(paper_rolls[0])
     for row_ind, row in enumerate(paper_rolls):
         for col_in, cell in enumerate(row):
             if cell == '@':
@@ -13,11 +14,10 @@ def main(input):
                 for dx,dy in [(-1,0),(1,0),(0,-1),(0,1), (-1,-1),(1,1),(-1,1),(1,-1)]:
                     row_check = row_ind + dy
                     col_check = col_in + dx
-                    if 0 <= row_check < len(paper_rolls) and 0 <= col_check < len(row):
+                    if 0 <= row_check < max_row_ind and 0 <= col_check < max_col_ind:
                         if paper_rolls[row_ind + dy][col_in + dx] == '@':
                             neighbors += 1
                 if neighbors < 4:
-                    print(f"found at row {row_ind} col {col_in} with few neighbors: {neighbors}")
                     result += 1
 
     return result
--- 2025/04/part2.py
+++ 2025/04/part2.py
@@ -2,33 +2,26 @@
 def parse_input_map(input):
     return [list(x) for x in input.splitlines()]
 
-def remove_roles_of_papers(paper_rolls):
-    result = 0
-    for row_ind, row in enumerate(paper_rolls):
-        for col_in, cell in enumerate(row):
-            if cell == "@":
-                neighbors = 0
-                for dx,dy in [(-1,0),(1,0),(0,-1),(0,1), (-1,-1),(1,1),(-1,1),(1,-1)]:
-                    row_check = row_ind + dy
-                    col_check = col_in + dx
-                    if 0 <= row_check < len(paper_rolls) and 0 <= col_check < len(row):
-                        if paper_rolls[row_ind + dy][col_in + dx] == "@":
-                            neighbors += 1
-                if neighbors < 4:
-                    print(
-                        f"found at row {row_ind} col {col_in} with few neighbors: {neighbors}"
-                    )
-                    result += 1
-                    paper_rolls[row_ind][col_in] = "#"
-    return result
-
-
 def main(input):
     paper_rolls = parse_input_map(input)
-    print(paper_rolls)
     result = 0
+    max_row_ind = len(paper_rolls)
+    max_col_ind = len(paper_rolls[0])
     while True:
-        removed = remove_roles_of_papers(paper_rolls)
+        removed = 0
+        for row_ind, row in enumerate(paper_rolls):
+            for col_in, cell in enumerate(row):
+                if cell == "@":
+                    neighbors = 0
+                    for dx,dy in [(-1,0),(1,0),(0,-1),(0,1), (-1,-1),(1,1),(-1,1),(1,-1)]:
+                        row_check = row_ind + dy
+                        col_check = col_in + dx
+                        if 0 <= row_check < max_row_ind and 0 <= col_check < max_col_ind:
+                            if paper_rolls[row_ind + dy][col_in + dx] == "@":
+                                neighbors += 1
+                    if neighbors < 4:
+                        removed += 1
+                        paper_rolls[row_ind][col_in] = "#"
         if removed == 0:
             break
         result += removed
