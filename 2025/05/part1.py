a35a5aa9a20a567d457490da1348d2ec7363c248
benganellison <bengt.ellison@gmail.com>
2025-12-05 06:40:19 +0100

    day 05 part 1

---
+++ 2025/05/part1.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+
+def parse_input_integers(input):
+    return [int(x) for x in input.splitlines()]
+
+
+def parse_input_pairs(input):
+    pairs = [x.split("-") for x in input.splitlines()]
+    return [(int(x), int(z)) for x, z in pairs]
+
+
+def main(input):
+
+    fresh_id_ranges = parse_input_pairs(input.split("\n\n")[0])
+    available_ingredients = parse_input_integers(input.split("\n\n")[1])
+
+    result = 0
+    for ingredient in available_ingredients:
+        for fresh_id_range in fresh_id_ranges:
+            if ingredient >= fresh_id_range[0] and ingredient <= fresh_id_range[1]:
+                result += 1
+                break
+
+    return result
+
+
+if __name__ == "__main__":
+    # Read the strategy from the input file.
+    with open('input.txt') as f:
+        input = f.read()
+
+    # Calculate and print the total score for the strategy.
+    result = main(input)
+    print("final result: ", result)
