dbefc2a190f00a507e68c0729004cae002a25bdd
benganellison <bengt.ellison@gmail.com>
2025-12-05 10:59:14 +0100

    clean up template

--- 2025/template/code_template.py
+++ 2025/template/code_template.py
@@ -1,19 +1,28 @@
 #! /usr/bin/env python
+def parse_input_pairs_of_int(instr):
+    return [tuple(int(x) for x in range.split("-")) for range in instr.splitlines()]
+
+
 def parse_input_integers(input):
     return [int(x) for x in input.splitlines()]
+
 
 def parse_input_strings(input):
     return input.splitlines()
 
+
 def parse_input_map(input):
     return [list(x) for x in input.splitlines()]
+
 
 def parse_input_grid_of_int(input):
     return [[int(x) for x in y] for y in input.splitlines()]
 
+
 def parse_input_tuples(input):
-    return [tuple(map(int, x.split(',')) for x in input.splitlines())]
-            
+    return [tuple(map(int, x.split(",")) for x in input.splitlines())]
+
+
 def main(input):
     result = ""
     return result
@@ -21,7 +30,7 @@ def main(input):
 
 if __name__ == "__main__":
     # Read the strategy from the input file.
-    with open('input.txt') as f:
+    with open("input.txt") as f:
         input = f.read()
 
     # Calculate and print the total score for the strategy.
